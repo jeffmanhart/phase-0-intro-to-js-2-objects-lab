8dbbd4ffaf0146f35e0588c9bf29ac7389fe2b5d
jeffmanhart <jeffreymanhart@gmail.com>
2023-08-10 11:50:32 -0600

    created functions to update, delete employees

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name : "",
+    streetAddress : ""
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const deleteEmployee = {...employee};
+    delete deleteEmployee[key];
+    return deleteEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
